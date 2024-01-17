345d7fa7d13380fd12c14cd05e52b3b154f3b68e
SvdNeo <sanjay.d@neoito.com>
2024-01-17 14:11:28 +0530

    created function add entry to create the dynamic input elements in each fielset

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ document.getElementById('calorieForm').addEventListener('submit', function (even
 
     addEntry();
   });
+  function addEntry() {
+    const entryType = document.getElementById('entryType').value;
+    const entryFieldsContainer = document.getElementById(entryType + 'Fieldset').querySelector('.input-container');
+
+    const entryNumber = entryFieldsContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+      <label for="${entryType}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+      <input type="text" id="${entryType}-${entryNumber}-name" placeholder="Name" />
+      <label for="${entryType}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+      <input
+        type="number"
+        min="0"
+        id="${entryType}-${entryNumber}-calories"
+        placeholder="Calories"
+      />`;
+
+    entryFieldsContainer.insertAdjacentHTML('beforeend', HTMLString);
+  }
